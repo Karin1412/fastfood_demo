21a1fc057e63f89a745a2ae847038af4617906e2
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2023-12-23 01:34:02 +0700

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import Login from './pages/auth/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
